67c10ea47f06fa788a7e44969ece3b3065143e75
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-07-23 22:36:27 +0200

    database helper functions

--- dynamo.js
+++ dynamo.js
@@ -1,3 +1,8 @@
+/***
+ * DATABASE CONNECTION AND HELPER FUNCTIONS
+***/
+
+//DEPENDENCIES
 const AWS = require('aws-sdk');
 require('dotenv').config(); //allows access to the credentials in .env
 
@@ -13,3 +18,28 @@ const dynamoClient = new AWS.DynamoDB.DocumentClient();
 
 //TABLE TO CONNECT TO
 const TABLE_NAME="conversationingerman-api";
+
+//GET ALL CONVERSATIONS
+const getTopics = async ()=> {
+    const params = {
+        TableName: TABLE_NAME
+    };
+    const topics = await dynamoClient.scan(params).promise();
+    return topics;
+}
+
+//GET A SINGLE CONVERSATION 
+const getTopicById = async (id)=> {
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {id}
+    };
+    const topic = await dynamoClient.get(params).promise();
+    return topic
+}
+
+module.exports = {
+    dynamoClient,
+    getTopics,
+    getTopicById
+}
